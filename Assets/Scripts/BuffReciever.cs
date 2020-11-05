20c68866e0737b85fcb9aa6a2ee5d89b1fce749a
MaximRezvanov <maximrezvanov@gmail.com>
2020-11-06 00:07:49 +0300

    adde actional delegate

---
+++ Assets/Scripts/BuffReciever.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BuffReciever : MonoBehaviour
+{
+
+    private List<Buff> buffs;
+    public Action OnBuffsChanged;
+
+
+    private void Start()
+    {
+        GameManager.Instance.buffRecieverContainer.Add(gameObject, this);
+        buffs = new List<Buff>();
+    }
+
+    public void AddBuff(Buff buff)
+    {
+        if (!buffs.Contains(buff))
+            buffs.Add(buff);
+
+        OnBuffsChanged?.Invoke();
+
+    }
+
+    public void RemoteBuff(Buff buff)
+    {
+        if (buffs.Contains(buff))
+            buffs.Remove(buff);
+
+        OnBuffsChanged?.Invoke();
+
+    }
+
+}
+
+
---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    private Item item;
+
+
+    private void Awake()
+    {
+        icon.sprite = null;
+    }
+
+    public void Init(Item item)
+    {
+        this.item = item;
+        icon.sprite = item.Icon;
+    }
+
+    public void OnClickCell() { 
+
+        if (item == null)
+        return;
+        GameManager.Instance.inventory.Items.Remove(item);
+        Buff buff = new Buff
+        {
+            type = item.Type,
+            additiveBonus = item.Value
+        };
+        GameManager.Instance.inventory.buffReciever.AddBuff(buff);
+        
+
+    }
+   
+}
